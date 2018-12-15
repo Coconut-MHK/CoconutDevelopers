30ba02160576a4b1921690a21dac68457699ffe2
Yung Ju Rick Kim <yungjurick@gmail.com>
2018-12-15 16:13:20 +0900

    Minor Edits

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,17 @@
 </template>
 
 <style lang="scss">
-@import './assets/css/App.scss';
+@import "./assets/css/App.scss";
 #app {
   width: 100vw;
   height: 100vh;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+a:link {
+  color: white;
 }
 </style>
